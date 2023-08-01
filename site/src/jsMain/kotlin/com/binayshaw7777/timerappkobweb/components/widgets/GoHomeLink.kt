082cc05d8df921a900d044fda40038afaabb69c7
Binay Shaw <binayshaw7777@gmail.com>
2023-08-01 13:55:21 +0530

    Initial Commit

---
+++ site/src/jsMain/kotlin/com/binayshaw7777/timerappkobweb/components/widgets/GoHomeLink.kt
@@ -0,0 +1,7 @@
+package com.binayshaw7777.timerappkobweb.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.silk.components.navigation.Link
+
+@Composable
+fun GoHomeLink() = Link("/", "Go Home")
